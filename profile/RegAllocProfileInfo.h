81be4d09cd52c961231adbf6f2ef32f124f0f99b
robertshih <robertshih@scotty.(none)>
2013-04-17 23:24:26 -0400

    refactored into main driver, class impl, header

---
+++ profile/RegAllocProfileInfo.h
@@ -0,0 +1,26 @@
+/*
+ * RegAllocProfileInfo.h
+ *
+ *  Created on: Apr 17, 2013
+ *      Author: robertshih
+ */
+
+#ifndef REGALLOCPROFILEINFO_H_
+#define REGALLOCPROFILEINFO_H_
+#include "llvm/Analysis/ProfileInfo.h"
+namespace llvm{
+class RegAllocProfileInfo: public ProfileInfo {
+  std::string Filename;
+  Module *M;
+  void readEdge
+    (ProfileInfo::Edge e,
+     std::vector<unsigned> &ECs,
+     unsigned& ReadCount);
+public:
+  RegAllocProfileInfo
+    (Function* F, const std::string &filename = "llvmprofile.out");
+  void dump();
+};
+}
+
+#endif /* REGALLOCPROFILEINFO_H_ */
